fb011c198c3ef917062fa02119583d2b28a3003d
w7b <pessoal.gabriel21@gmail.com>
2025-02-01 21:34:10 -0300

    Commit inicial com esqueleto

---
+++ src/test/java/com/smoothy/agendadorTarefas/AgendadorTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smoothy.agendadorTarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadorTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
